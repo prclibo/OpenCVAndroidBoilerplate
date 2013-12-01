cd54c7bbe8aa2b82129a1733bc69db6982b9e673
Bo Li <prclibo@gmail.com>
2013-11-30 21:21:20 -0800

    Added basic code using OpenCV

--- src/com/example/opencvandroidboilerplate/MainActivity.java
+++ src/com/example/opencvandroidboilerplate/MainActivity.java
@@ -1,15 +1,89 @@
 package com.example.opencvandroidboilerplate;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements CvCameraViewListener2 {
+	final String TAG = "Rectangle";
+	private CameraBridgeViewBase mOpenCvCameraView;
+
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+	    @Override
+	    public void onManagerConnected(int status) {
+	        switch (status) {
+	            case LoaderCallbackInterface.SUCCESS:
+	            {
+	                Log.i(TAG, "OpenCV loaded successfully");
+	                System.loadLibrary("process_frame");
+	                
+	                mOpenCvCameraView.enableView();
+	            } break;
+	            default:
+	            {
+	                super.onManagerConnected(status);
+	            } break;
+	        }
+	    }
+	};
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public void onResume()
+	{
+	    super.onResume();
+	    OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    Log.i(TAG, "called onCreate");
+	    super.onCreate(savedInstanceState);
+	    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	    setContentView(R.layout.activity_main);
+	    mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.CameraView	);
+	    mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+	    mOpenCvCameraView.setCvCameraViewListener(this);
+	}
+
+	@Override
+	public void onPause()
+	{
+	    super.onPause();
+	    if (mOpenCvCameraView != null)
+	        mOpenCvCameraView.disableView();
+	}
+
+	public void onDestroy() {
+	    super.onDestroy();
+	    if (mOpenCvCameraView != null)
+	        mOpenCvCameraView.disableView();
+	}
+
+	public void onCameraViewStarted(int width, int height) {
+	}
+
+	public void onCameraViewStopped() {
+	}
+
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+		Mat mat = new Mat();
+		Mat input = inputFrame.rgba();
+		
+		processFrame(input.getNativeObjAddr(), mat.getNativeObjAddr(), 
+				input.height(), input.width());
+	    return mat;
+	    
 	}
 
 	@Override
@@ -19,4 +93,5 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public native void processFrame(long matAddrInRGBA, long matAddrOutInRGBA, int height, int width);
 }
